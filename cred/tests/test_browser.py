78eb07646d4000edd7d313a0ea552e635fe0c06c
Toshiaki Baba <toshiaki@netmark.jp>
2017-04-13 15:06:09 +0900

    trying to avoid test fail test_password_generator

--- cred/tests/test_browser.py
+++ cred/tests/test_browser.py
@@ -40,7 +40,7 @@ class JavascriptTests(LiveServerTestCase):
         super(JavascriptTests, cls).tearDownClass()
 
     def waitforload(self):
-        timeout = 4
+        timeout = 10
         WebDriverWait(self.selenium, timeout).until(
             lambda driver: driver.find_element_by_tag_name('body'))
 
